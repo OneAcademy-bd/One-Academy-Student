3843d88db240cec20cf464dbe15717437cd5ca15
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2024-11-08 14:16:51 +0600

    update for render api_base

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,11 +1,14 @@
 // local in this system
-const API_BASE = 'http://127.0.0.1:8000'
+// const API_BASE = 'http://127.0.0.1:8000'
 
 // local in this LAN
 // const API_BASE = 'http://192.168.1.115:8000'
 
 // aws
 // const API_BASE = 'http://13.126.56.249:8000'
+
+// render
+const API_BASE = 'https://one-academy-backend-we62.onrender.com'
 
 export const endpoints = {
     profile: `${API_BASE}/student/api/profile/`,
